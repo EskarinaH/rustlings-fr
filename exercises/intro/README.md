65cb0b00a9d5efa82146d3540124231125051e25
EskarinaH <eskarina.h@unseen-university.akhpk>
2022-05-27 12:31:37 +0200

    Traduit intro/intro1

--- exercises/intro/README.md
+++ exercises/intro/README.md
@@ -1,8 +1,8 @@
 # Intro
 
-Rust uses the `print!` and `println!` macros to print text to the console.
+Rust utilise les macros `print!` et `println!` pour afficher du texte dans la console.
 
-## Further information
+## Pour plus d'information
 
-- [Hello World](https://doc.rust-lang.org/rust-by-example/hello.html)
-- [Formatted print](https://doc.rust-lang.org/rust-by-example/hello/print.html)
+- [Hello World](https://doc.rust-lang.org/rust-by-example/hello.html) (en anglais)
+- [Formatted print](https://doc.rust-lang.org/rust-by-example/hello/print.html) (en anglais)
--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -1,23 +1,26 @@
 // intro1.rs
-// About this `I AM NOT DONE` thing:
-// We sometimes encourage you to keep trying things on a given exercise, even
-// after you already figured it out. If you got everything working and feel
-// ready for the next exercise, remove the `I AM NOT DONE` comment below.
-// Execute the command `rustlings hint intro1` for a hint.
+// Au sujet des commentaires `I AM NOT DONE` :
+// Nous vous encourageons parfois à continuer à tester des choses sur un exercice
+// donné, même après que vous l'ayez résolu. Si tout fonctionne et que vous sentez
+// qu'il est temps de passer à l'exercice suivant, supprimez le commentaire
+// `I AM NOT DONE` ci-dessous (littéralement "JE N'AI PAS FINI").
+// Exécutez `rustlings hint intro1` pour avoir un conseil.
 
 // I AM NOT DONE
 
 fn main() {
-    println!("Hello and");
-    println!(r#"       welcome to...                      "#);
+    println!("Bonjour et");
+    println!(r#"       bienvenue dans...                  "#);
     println!(r#"                 _   _ _                  "#);
     println!(r#"  _ __ _   _ ___| |_| (_)_ __   __ _ ___  "#);
     println!(r#" | '__| | | / __| __| | | '_ \ / _` / __| "#);
     println!(r#" | |  | |_| \__ \ |_| | | | | | (_| \__ \ "#);
     println!(r#" |_|   \__,_|___/\__|_|_|_| |_|\__, |___/ "#);
     println!(r#"                               |___/      "#);
+    println!(r#"                                          "#);
+    println!(r#"                          ...en français !"#);
     println!();
-    println!("This exercise compiles successfully. The remaining exercises contain a compiler");
-    println!("or logic error. The central concept behind Rustlings is to fix these errors and");
-    println!("solve the exercises. Good luck!");
+    println!("Cet exercice compile sans problème. Les autres exercices contiennent une erreur de");
+    println!("compilation ou de logique. Le concept de base derrière Rustlings est de corriger");
+    println!("ces erreurs et de résoudre les exercices. Bonne chance !");
 }
